fd0d351c05b398c349a19f723a83faa24b83ec87
Alireza Askari <askarialireza373@gmail.com>
2020-03-23 18:34:42 +0430

    Updated DatabaseConnection.cs and EmploymentTypeService.cs

--- ChargoonTestApplication/Infrastructure/DatabaseConnection.cs
+++ ChargoonTestApplication/Infrastructure/DatabaseConnection.cs
@@ -7,16 +7,9 @@ namespace Infrastructure
         static DatabaseConnection()
         {
             ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ChargoonTestDB"].ConnectionString;
-
-            if (Connection == null)
-            {
-                Connection = new System.Data.SqlClient.SqlConnection(ConnectionString);
-            }
         }
 
         public static string ConnectionString { get; set; }
 
-
-        public static System.Data.SqlClient.SqlConnection Connection { get; set; }
     }
 }
--- ChargoonTestApplication/Services/EmploymentTypeService.cs
+++ ChargoonTestApplication/Services/EmploymentTypeService.cs
@@ -1,4 +1,6 @@
-﻿namespace Services
+﻿using Dapper;
+
+namespace Services
 {
     public static class EmploymentTypeService : System.Object
     {
@@ -9,109 +11,40 @@
 
         public static string GetNameById(int id)
         {
-            string result = string.Empty;
 
-            if (Infrastructure.DatabaseConnection.Connection.State == System.Data.ConnectionState.Closed)
+            using (var connection = new System.Data.SqlClient.SqlConnection(Infrastructure.DatabaseConnection.ConnectionString))
             {
-                Infrastructure.DatabaseConnection.Connection.Open();
+                string result = string.Empty;
+
+                result = connection.QueryFirstOrDefault<string>
+                    ("GetEmploymentTypeNameById", param: new { Id = id }, commandType: System.Data.CommandType.StoredProcedure);
+
+                return result;
             }
-
-            System.Data.SqlClient.SqlCommand oSqlCommand =
-                new System.Data.SqlClient.SqlCommand("GetEmploymentTypeNameById", Infrastructure.DatabaseConnection.Connection);
-
-            oSqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
-
-            oSqlCommand.Parameters.AddWithValue("@Id", id);
-
-            System.Data.SqlClient.SqlDataReader oSqlDataReader = oSqlCommand.ExecuteReader();
-
-            if (oSqlDataReader.HasRows)
-            {
-                while (oSqlDataReader.Read())
-                {
-                    result = oSqlDataReader["Name"].ToString();
-                }
-            }
-
-            oSqlCommand.Dispose();
-
-            Infrastructure.DatabaseConnection.Connection.Close();
-
-            return result;
         }
 
         public static int GetIdByName(string name)
         {
-            int result = 0;
-
-            if (Infrastructure.DatabaseConnection.Connection.State == System.Data.ConnectionState.Closed)
+            using (var connection = new System.Data.SqlClient.SqlConnection(Infrastructure.DatabaseConnection.ConnectionString))
             {
-                Infrastructure.DatabaseConnection.Connection.Open();
+                int result = 0;
+
+                result = connection.QueryFirstOrDefault<int>
+                    ("GetEmploymentTypeIdByName", param: new { Name = name }, commandType: System.Data.CommandType.StoredProcedure);
+
+                return result;
             }
-
-            System.Data.SqlClient.SqlCommand oSqlCommand =
-                new System.Data.SqlClient.SqlCommand("GetEmploymentTypeIdByName", Infrastructure.DatabaseConnection.Connection);
-
-            oSqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
-
-            oSqlCommand.Parameters.AddWithValue("@Name", name);
-
-            System.Data.SqlClient.SqlDataReader oSqlDataReader = oSqlCommand.ExecuteReader();
-
-            if (oSqlDataReader.HasRows)
-            {
-                while (oSqlDataReader.Read())
-                {
-                    result = System.Convert.ToInt32(oSqlDataReader["Id"].ToString());
-                }
-            }
-
-            oSqlCommand.Dispose();
-
-            Infrastructure.DatabaseConnection.Connection.Close();
-
-            return result;
         }
 
-        public static System.Collections.Generic.List<Models.EmploymentType> GetAll()
+        public static System.Collections.Generic.IEnumerable<Models.EmploymentType> GetAll()
         {
-            System.Collections.Generic.List<Models.EmploymentType> employmentTypes = new System.Collections.Generic.List<Models.EmploymentType>();
-
-            if (Infrastructure.DatabaseConnection.Connection.State == System.Data.ConnectionState.Closed)
+            using (var connection = new System.Data.SqlClient.SqlConnection(Infrastructure.DatabaseConnection.ConnectionString))
             {
-                Infrastructure.DatabaseConnection.Connection.Open();
+                var varList = connection.Query<Models.EmploymentType>
+                    ("GetEmploymentTypes", commandType: System.Data.CommandType.StoredProcedure);
+
+                return varList;
             }
-
-
-            System.Data.SqlClient.SqlCommand oSqlCommand =
-                new System.Data.SqlClient.SqlCommand("GetEmploymentTypes", Infrastructure.DatabaseConnection.Connection);
-
-            oSqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
-
-            System.Data.SqlClient.SqlDataReader oSqlDataReader = oSqlCommand.ExecuteReader();
-
-            Models.EmploymentType oEmploymentType = null;
-
-            if (oSqlDataReader.HasRows)
-            {
-                while (oSqlDataReader.Read())
-                {
-                    oEmploymentType = new Models.EmploymentType();
-
-                    oEmploymentType.Id = System.Convert.ToInt32(oSqlDataReader["Id"]);
-                    oEmploymentType.IsActive = System.Convert.ToBoolean(oSqlDataReader["IsActive"]);
-                    oEmploymentType.Name = oSqlDataReader["Name"].ToString();
-
-                    employmentTypes.Add(oEmploymentType);
-                }
-            }
-
-            oSqlCommand.Dispose();
-
-            Infrastructure.DatabaseConnection.Connection.Close();
-
-            return employmentTypes;
-
         }
     }
 }

1a5ab19532577c84a0ac9e2f763d9b2b1b9ebfa6
Alireza Askari <askarialireza373@gmail.com>
2020-03-27 15:25:22 +0430

    Added Database Script Files.

---
+++ ChargoonTestApplication/SQL/seed-employmentTypes.sql
@@ -0,0 +1,14 @@
+﻿USE [ChargoonTestDB]
+GO
+
+INSERT INTO [dbo].[EmploymentTypes] ([Name] ,[IsActive])
+     VALUES ( N'تمام وقت' , 1)
+GO
+
+INSERT INTO [dbo].[EmploymentTypes] ([Name] ,[IsActive])
+     VALUES ( N'پاره وقت' , 1)
+GO
+
+INSERT INTO [dbo].[EmploymentTypes] ([Name] ,[IsActive])
+     VALUES ( N'سایر' , 1)
+GO
